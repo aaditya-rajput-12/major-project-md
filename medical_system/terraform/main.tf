62cc74eca63e1b084ec470dd1b1ca7b28fa610c7
aadigit1 <aadityarajput7394@gmail.com>
2026-06-29 16:12:00 +0530

    Add Terraform configuration

---
+++ medical_system/terraform/main.tf
@@ -0,0 +1,80 @@
+module "vpc" {
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  enable_nat_gateway = true
+
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "production"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+
+    workers = {
+
+      instance_types = [var.instance_type]
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      disk_size = 20
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "production"
+    Terraform   = "true"
+  }
+}
---
+++ medical_system/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "production-cluster"
+}
+
+variable "cluster_version" {
+  default = "1.30"
+}
+
+variable "instance_type" {
+  default = "m7i-flex.large"
+}
